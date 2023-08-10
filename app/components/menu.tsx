aa65d92fae8fd9e4fdbbbd8018e97ad2fcd49bb9
Rakesh <rakesh.synswift@gmail.com>
2023-08-10 12:08:08 +0530

    update

---
+++ app/components/menu.tsx
@@ -0,0 +1,28 @@
+import {
+  Menubar,
+//   MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+//   MenubarLabel,
+  MenubarMenu,
+//   MenubarRadioGroup,
+//   MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+//   MenubarSub,
+//   MenubarSubContent,
+//   MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+
+export function Menu() {
+  return (<>
+    <h1>menu</h1>
+    <Menubar className="rounded-none border-b border-none px-2 lg:px-4">
+
+
+    </Menubar>
+  </>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import { NextPage } from "next";
+
+const DashboardPage:NextPage=()=>{
+return(<h1>hi</h1>)
+}
+export default DashboardPage;
